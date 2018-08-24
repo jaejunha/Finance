2158212c1b6fc4343f602cb164cd5a1a371ebe89
jaejunha <dreamline91@naver.com>
2018-08-24 21:39:41 +0900

    fetch list of kospi from db

--- dbms.py
+++ dbms.py
@@ -54,22 +54,34 @@ def checkToday(db_id, db_pwd, db_db):
 
 
 def updateToday(db_id, db_pwd, db_db, list_kospi200):
+	int_today = getToday()
 	sql_con = pymysql.connect(host = CONST_HOST, user = db_id, password = db_pwd, db = db_db, charset = CONST_CHARSET)
 	sql_cur = sql_con.cursor()
-	sql_query = "insert into date_today values('"+getToday()+"')"
+	sql_query = "insert into date_today values('"+int_today+"')"
 	sql_cur.execute(sql_query)
 	sql_con.commit()
 	
 	for item_kospi200 in list_kospi200:
-		sql_query = "insert into list_kospi200 values("+item_kospi200[0]+",'"+item_kospi200[1].decode('euc-kr')+"')"
+		sql_query = "insert into list_kospi200 values("+item_kospi200[0]+",'"+item_kospi200[1].decode('euc-kr')+"', "+getToday()+")"
 		try:
 			sql_cur.execute(sql_query)
 		except pymysql.err.ProgrammingError as e:
 			if(str(e).find("doesn't exist") >= 0):	
-				sql_query = "create table list_kospi200(int_code int, str_name varchar(40), primary key(int_code)) default charset="+CONST_CHARSET
+				sql_query = "create table list_kospi200(int_code int primary key, str_name varchar(40), f_date int, foreign key(f_date) references date_today(int_date) on delete cascade) default charset="+CONST_CHARSET
 				sql_cur.execute(sql_query)
 				sql_con.commit()
-				sql_query = "insert into list_kospi200 values("+item_kospi200[0]+",'"+item_kospi200[1].decode('euc-kr')+"')"
+				sql_query = "insert into list_kospi200 values("+item_kospi200[0]+",'"+item_kospi200[1].decode('euc-kr')+"', "+int_today+")"
 				sql_cur.execute(sql_query)
 	sql_con.commit()
 	sql_con.close()
+
+def getKOSPI200(db_id, db_pwd, db_db):
+	list_kospi200 = []
+	sql_con = pymysql.connect(host = CONST_HOST, user = db_id, password = db_pwd, db = db_db, charset = CONST_CHARSET)
+	sql_cur = sql_con.cursor()
+	sql_query = "select * from list_kospi200 where f_date = " + getToday()
+	sql_cur.execute(sql_query)
+	for row in sql_cur:
+		list_kospi200.append((str(row[0]), row[1]))
+	sql_con.close()
+	return list_kospi200
--- start.py
+++ start.py
@@ -1,45 +1,22 @@
-import random
-
 import util as UTIL
+import menu as MENU
 import dbms as DBMS
-import http as HTTP
 
 if __name__ == '__main__':
+	bool_update = False
+	list_kospi200 = []
 	db_id, db_pwd, db_db, bool_error = DBMS.checkDB()
-	bool_start = False
+
 	while bool_error == False:
 		UTIL.printMenu()
 		str_menu = raw_input('Please select the MENU >> ')
 		if str_menu == '1':
-			if DBMS.checkToday(db_id, db_pwd, db_db) == False:
-				list_kospi200 = HTTP.getKOSPI200(db_id, db_pwd, db_db)
-			elif bool_start == False:
-				# fetch list from db
-				bool_start = True
-			str_answer = raw_input('Would you want to see the list of KOSPI 200?(yes/no) >> ')
-			if str_answer == 'yes' or str_answer == 'YES':
-				int_i = 1
-				for item_kospi200 in list_kospi200:
-					print str(int_i) + '. ' +item_kospi200[0] + ' ' + item_kospi200[1]
-					int_i += 1
-			int_size = len(list_kospi200)
-			list_random = []
-			str_answer = raw_input('How many you want to choose? >> ')
-			int_random = int(str_answer)
-
-#Need to try catch statement(wrong input)
-#Need to try catch statement(out of range)
-			while len(list_random) < int_random:
-				item_kospi200 = list_kospi200[random.randrange(0, int_size + 1)]
-				if item_kospi200 in list_random:
-					pass
-				else:
-					list_random.append(item_kospi200)
-			int_i = 1
-			for item_random in list_random:
-				print str(int_i) + '. ' +item_random[0] + ' ' + item_random[1]
-				int_i += 1
-			str_answer = raw_input('Please Enter the any key')
-		else:
+			bool_update, list_kospi200 = MENU.randomKOSPI200(db_id, db_pwd, db_db, bool_update, list_kospi200)
+		elif str_menu == '2':
 			print 'Good Bye!'
 			break
+		else:
+			print 'Check your input'
+			str_answer = raw_input('Please Enter the any key')
+
+
