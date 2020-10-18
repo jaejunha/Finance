cb4db113060ee0817b0444d6fb7159528369c21c
jaejunha <dreamline91@naver.com>
2020-10-18 21:56:31 +0900

    refactoring

--- reader.py
+++ reader.py
@@ -8,35 +8,47 @@ UNIT_ORIGIN = 50
 # For Samsung Electronics
 # argv[1]: csv file
 """
-dic_data = {}
-list_date = []
-file_r = open(sys.argv[1], "r")
-file_w = open("result.csv", "w")
-for line in file_r.readlines():
-	list_raw = line.strip().split(",")
 
-	date = int(list_raw[0])
-	list_date.append(date)
+def readRawData():
+	dic_data = {}
+	list_date = []
 
-	close = int(list_raw[1])
-	open = int(list_raw[2])
-	high = int(list_raw[3])
-	low = int(list_raw[4])
+	file = open(sys.argv[1], "r")
+	for line in file.readlines():
+		list_raw = line.strip().split(",")
 
-	if (date >= DATE_DIVISION) and (close < UNIT_MILLION):
-		close *= UNIT_ORIGIN
-		open *= UNIT_ORIGIN
-		high *= UNIT_ORIGIN
-		low *= UNIT_ORIGIN
+		date = int(list_raw[0])
+		list_date.append(date)
 
-	dic_data[date] = {"close": close, "open": open, "high": high, "low": low}
-file_r.close()
+		end = int(list_raw[1])
+		start = int(list_raw[2])
+		high = int(list_raw[3])
+		low = int(list_raw[4])
 
-list_date.sort()
+		if (date >= DATE_DIVISION) and (end < UNIT_MILLION):
+			end *= UNIT_ORIGIN
+			start *= UNIT_ORIGIN
+			high *= UNIT_ORIGIN
+			low *= UNIT_ORIGIN
 
-"""
-Below line will be deleted
-"""
-file_w.write("날짜, 종가, 시가, 고가, 저가\n")
-for date in list_date:
-	file_w.write("%d, %d, %d, %d, %d\n" % (date, dic_data[date]["close"], dic_data[date]["open"], dic_data[date]["high"], dic_data[date]["low"]))
+		dic_data[date] = {"end": end, "start": start, "high": high, "low": low}
+	file.close()
+
+	list_date.sort()
+
+	return list_date, dic_data
+
+def saveModifiedData(list_date, dic_data):
+	code = sys.argv[1].split("_")[0]
+	file = open(code + "_data.csv", "w")
+
+	"""
+	Below line will be deleted
+	"""
+	file.write("날짜, 종가, 시가, 고가, 저가\n")
+	for date in list_date:
+		file.write("%d, %d, %d, %d, %d\n" % (date, dic_data[date]["end"], dic_data[date]["start"], dic_data[date]["high"], dic_data[date]["low"]))
+
+if __name__ == "__main__":
+	list_date, dic_data = readRawData()
+	saveModifiedData(list_date, dic_data)
