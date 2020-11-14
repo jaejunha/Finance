1d6692e24a6af089796166efae022de3f9637676
jaejunha <dreamline91@naver.com>
2020-11-15 01:15:41 +0900

    for distribution

--- value/main.py
+++ value/main.py
@@ -1,11 +1,12 @@
 import requests
 import re
+import os
 
 from datetime import datetime
 
 URL_FINANCE = "https://finance.naver.com"
 URL_SUM = URL_FINANCE + "/sise/sise_market_sum.nhn"
-URL_SUM_SET = URL_FINANCE + "/sise/field_submit.nhn?menu=market_sum&returnUrl=" + URL_SUM
+URL_SUM_SET = URL_FINANCE + "/sise/field_submit.nhn?menu=market_sum&returnUrl=http%3A%2F%2Ffinance.naver.com%2Fsise%2Fsise_market_sum.nhn"
 
 CONST_KOSPI = 0
 CONST_KOSDAQ = 1
@@ -20,6 +21,20 @@ CONST_PER = 8
 CONST_ROE = 9
 CONST_PBR = 10
 
+def getBlackList():
+	list_blacklist = []
+
+	try:
+		file = open("../_data/blacklist.txt", "r", encoding = "UTF8")
+		for line in file.readlines():
+			code = line.split(",")[0].strip()
+			list_blacklist.append(code)
+		file.close()
+	except FileNotFoundError:
+		pass
+	
+	return list_blacklist
+
 def getLastPage(sosk):
 	url = "%s?sosok=%d" % (URL_SUM, sosk)
 	
@@ -30,7 +45,8 @@ def getLastPage(sosk):
 			return int(re.findall("\d\d", line)[0])
 
 def getHTML(sosok, page):
-	url = "%s?sosok=%d&page=%d" % (URL_SUM_SET, sosok, page)
+	url = "%s?sosok=%d%%26page%%3D%d" % (URL_SUM_SET, sosok, page)
+	#%3FfieldIds%3Dmarket_sum%26page%3D5
 	url += "&fieldIds=market_sum"
 	url += "&fieldIds=debt_total"
 	url += "&fieldIds=operating_profit"
@@ -41,28 +57,11 @@ def getHTML(sosok, page):
 	res = requests.get(url)
 	return res.text.split("\t")
 
-if __name__ == "__main__":
-	#print(getLastPage(CONST_KOSPI))
-	#print(getLastPage(CONST_KOSDAQ))
-	
-	try:
-		file_blacklist = open("../_data/blacklist.txt", "r")
-	except FileNotFoundError:
-		pass
-
-	file_day = open("../_data/" + datetime.today().strftime("%Y%m%d") + ".txt", "w")
-
-	list_line = getHTML(CONST_KOSPI, 1)
+def saveData(file, list_line):
 	for i, line in enumerate(list_line):
 		if "/item/main.nhn?code=" in list_line[i]:
 			code = re.findall('=[^"]+', list_line[i])[0][1:]
 			name = re.findall('">.+</a>', list_line[i])[0][2: -4]
-			sum = None
-			dept = None
-			profit = None
-			per = None
-			roe = None
-			pbr = None
 
 			cnt_num = 0
 			while cnt_num < NUM_CNT:
@@ -103,5 +102,54 @@ if __name__ == "__main__":
 							pbr = float(res[0][1:-1].replace(",", ""))
 						else:
 							pbr = VAL_GARBAGE
+			if code in list_blacklist:
+				continue
+			file.write("%s, %s, %d, %d, %d, %f, %f, %f\n" % (code, name, sum, dept, profit, per, roe, pbr))
 			print(code, name, sum, dept, profit, per, roe, pbr)
+
+if __name__ == "__main__":
+
+	str_date = datetime.today().strftime("%Y%m%d")
+
+	if os.path.exists("../_data/" + str_date + ".txt") is False:
+	
+		list_blacklist = getBlackList()
+
+		file_day = open("../_data/" + str_date + ".txt", "w")
+		file_day.write("code, name, sum, dept, profit, per, roe, pbr\n")
+
+		for page in range(1, getLastPage(CONST_KOSPI) + 1):
+			list_line = getHTML(CONST_KOSPI, page)
+			saveData(file_day, list_line)
+
+		for page in range(1, getLastPage(CONST_KOSDAQ) + 1):
+			list_line = getHTML(CONST_KOSDAQ, page)
+			saveData(file_day, list_line)		
+		file_day.close()
+
+	file = open("temp.csv", "w")
+	file.write("이름, 시총, 영업이익\n")
+
+	list_sum = []
+	list_profit = []
+	file_day = open("../_data/" + str_date + ".txt", "r")
+	for i, line in enumerate(file_day.readlines()):
+		if i == 0:
+			continue
+		list_line = line.split(",")
+		code = list_line[0].strip()
+		name = list_line[1].strip()
+		sum = int(list_line[2].strip())
+		profit = int(list_line[4].strip())
+
+		list_sum.append( (sum, code) )
+		list_profit.append( (profit, code) )
+
+		# 적자 제외
+		if profit >= 0:
+			file.write("%s, %d, %d\n" % (name, sum, profit) )
+
+	list_sum.sort(reverse = True)
+	list_profit.sort(reverse = True)
 		
+	
