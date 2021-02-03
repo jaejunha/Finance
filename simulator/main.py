6e5744259f22a15d011c64001997850ab441674c
dreamline91 <dreamline91@naver.com>
2021-02-04 08:04:01 +0900

    Use class

--- simulator/main.py
+++ simulator/main.py
@@ -1,9 +1,14 @@
 from tkinter import *
 
+class Simulator:
+	def __init__(self, master):
+		self.master = master
+		master.title("Simulator")
+
+		self.label_hello = Label(master, text = "Hello World!")
+		self.label_hello.pack()
+
 if __name__ == "__main__":
 	window = Tk()
- 
-	lable_hello = Label(window, text = "hello world!")
-	lable_hello.pack()
- 
+	simulator = Simulator(window)
 	window.mainloop()
